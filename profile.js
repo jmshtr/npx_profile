d150791fab959689b68f4ff7a698fdf65c92ce3e
jmshtr <157889640+jmshtr@users.noreply.github.com>
2024-02-12 09:43:40 +0000

    Updated profile.js
    Removed unused const and fixed syntax.
    
    v. 1.1.0

--- profile.js
+++ profile.js
@@ -1,57 +1,49 @@
 #!/usr/bin/env node
 
-'use strict'
+'use strict';
 
-const boxen = require("boxen");
-const chalk = require("chalk");
-const inquirer = require("inquirer");
-const clear = require("clear");
-const open = require("open");
-const fs = require('fs');
-const request = require('request');
-const path = require('path');
-const ora = require('ora');
-const cliSpinners = require('cli-spinners');
-clear();
-
-const prompt = inquirer.createPromptModule();
+const boxen = require('boxen');
+const chalk = require('chalk');
+const clear = require('clear');
 
 const data = {
-    name: chalk.bold.green("              James Hunter"),
-    twitter: chalk.gray("https://twitter.com/") + chalk.cyan("jmshtr"),
-    github: chalk.gray("https://github.com/") + chalk.green("jmshtr"),
-    web: chalk.cyan("https://jmshtr.github.io"),
-    npx: chalk.red("               npx") + " " + chalk.white("jmshtr"),
+    name: chalk.bold.green('              James Hunter'),
+    twitter: chalk.gray('https://twitter.com/') + chalk.cyan('jmshtr'),
+    github: chalk.gray('https://github.com/') + chalk.green('jmshtr'),
+    web: chalk.cyan('https://jmshtr.github.io'),
+    npx: chalk.red('               npx') + ' ' + chalk.white('jmshtr'),
 
-    labelTwitter: chalk.white.bold("    Twitter:"),
-    labelGitHub: chalk.white.bold("     GitHub:"),
-    labelWeb: chalk.white.bold("        Web:"),
+    labels: {
+        twitter: chalk.white.bold('    Twitter:'),
+        github: chalk.white.bold('     GitHub:'),
+        web: chalk.white.bold('        Web:')
+    }
 };
 
 const aboutMe = boxen(
     [
         `${data.name}`,
-        ``,
-        `${data.labelTwitter}  ${data.twitter}`,
-        `${data.labelGitHub}  ${data.github}`,
-        `${data.labelWeb}  ${data.web}`,
-        ``,
+        '',
+        `${data.labels.twitter}  ${data.twitter}`,
+        `${data.labels.github}  ${data.github}`,
+        `${data.labels.web}  ${data.web}`,
+        '',
         `${data.npx}`
-    ].join("\n"),
+    ].join('\n'),
     {
         margin: 1,
         float: 'center',
         padding: 1,
-        borderStyle: "single",
-        borderColor: "green"
+        borderStyle: 'single',
+        borderColor: 'green'
     }
 );
 
+clear();
 console.log(aboutMe);
+
 const tip = [
-    `Tip: Try ${chalk.cyanBright.bold(
-        "cmd/ctrl + click"
-    )} on the links above`,
-    '',
-].join("\n");
+    `Tip: Try ${chalk.cyanBright.bold('cmd/ctrl + click')} on the links above`,
+    ''
+].join('\n');
 console.log(tip);
